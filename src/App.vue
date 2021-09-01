5d5d7ea7451487e21644bc05bfc23bd5bc5dfc47
dhadbs2 <dhadbs2@naver.com>
2021-09-01 14:35:33 +0900

    [update] firebase 연동

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,38 @@
       color="primary"
       dark
     >
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>Collapsing Bars</v-toolbar-title>
-    <v-spacer/>
-    <v-btn icon to="/about">
-        <v-icon>mdi-magnify</v-icon>
-    </v-btn>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <site-title :title="title"></site-title>
+      <v-spacer/>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-divider></v-divider>
+      <site-menu></site-menu>
+    </v-navigation-drawer>
     <v-content>
       <router-view/>
     </v-content>
-    <v-footer app color="primary" dark absolute>
-    <v-spacer></v-spacer>
-    <v-col
-      class="text-center"
-      cols="12"
-    >
-      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-    </v-col>
-    </v-footer>
+    <site-footer :footer="footer"></site-footer>
   </v-app>
 </template>
 
 <script>
+import SiteTitle from '@/views/site/title'
+import SiteFooter from '@/views/site/footer'
+import SiteMenu from '@/views/site/menu'
 
 export default {
-  name: 'App'
+  components: { SiteTitle, SiteFooter, SiteMenu },
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      items: [],
+      title: '나의 타이틀입니다',
+      footer: '푸터입니다'
+    }
+  },
+  mounted () {
+  }
 }
 </script>
---
+++ src/views/site/footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-footer app color="primary" dark absolute :footer="footer">
+    <v-spacer></v-spacer>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() + ' ' + footer }} — <strong>Vuefy</strong>
+    </v-col>
+    </v-footer>
+</template>
+<script>
+export default {
+  props: ['footer']
+}
+</script>
